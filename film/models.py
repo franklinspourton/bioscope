4edc32638fe9d0444090ef357cc875f7d80ebd15
franklin spourton <franklinspourton@gmail.com>
2019-03-26 20:26:32 +0530

    create a simple model

--- film/models.py
+++ film/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Film(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    film_name = models.CharField(max_length=20, blank=False, unique=True)
